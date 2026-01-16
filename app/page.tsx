3dbbbdbb566e6eb0005f63fb4bd340d5642928e7
Andrew Wladis <andrewwladis@gmail.com>
2026-01-16 16:18:29 -0700

    Removed blue tint

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SnowAnimation from './components/SnowAnimation';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-900 to-black relative overflow-hidden">
+    <div className="min-h-screen relative overflow-hidden">
       <SnowAnimation />
       <div className="relative z-10 min-h-screen py-8">
         <CalorieTracker />
